c2a982a83c5c6b9aa703252d29bb16afff128ce8
Abdulrahman Hussain <abdulrahman_babiker@outlook.com>
2026-02-03 18:01:06 +0400

    Add Carter, Mapster, MediatR integration and endpoint setup
    Integrated Carter, Mapster, and MediatR via NuGet packages. Refactored CreateProductEndpoint to use ICarterModule and mapped the POST /products endpoint with request/response mapping and MediatR command dispatch. Added CarterExtensions for dynamic endpoint registration. Updated Program.cs for service registration and endpoint mapping. Introduced global usings for cleaner code.

---
+++ src/BuildingBlocks/BuildingBlocks/Extensions/CarterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Carter;
+
+namespace BuildingBlocks.Extensions;
+
+public static class CarterExtensions
+{
+    public static void RegisterEndpointsFromAssemblies(this CarterConfigurator config, Assembly[] assemblies)
+    {
+        foreach (var assembly in assemblies)
+        {
+            var typesImplementingICarterModule = assembly.GetTypes()
+                .Where(type => typeof(ICarterModule).IsAssignableFrom(type) && !type.IsAbstract)
+                .ToArray();
+
+            config.WithModules(typesImplementingICarterModule);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,23 @@
+using System.Reflection;
+using BuildingBlocks.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter(configurator: config =>
+{
+    config.RegisterEndpointsFromAssemblies([typeof(Program).Assembly]);
+});
 
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
+
 
 app.Run();
