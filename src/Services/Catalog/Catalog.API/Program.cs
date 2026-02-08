5ba89d2a7db2e4c5496d159a90d1b6f4974708bd
Abdulrahman Hussain <abdulrahman_babiker@outlook.com>
2026-02-08 17:58:38 +0400

    Add Swagger

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,11 @@
-using System.Reflection;
 using BuildingBlocks.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddCarter(configurator: config =>
 {
     config.RegisterEndpointsFromAssemblies([typeof(Program).Assembly]);
@@ -18,6 +20,16 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = string.Empty; // Makes Swagger UI the root page
+    });
+}
 
 
 app.Run();
