8bb8ab658b9b1a25dca710ceb199db2a871f38da
Abdulrahman Hussain <abdulrahman_babiker@outlook.com>
2026-04-19 17:50:45 +0400

    Add Discount.Grpc microservice with gRPC and EF Core
    Added Discount.Grpc project with gRPC endpoints and EF Core context for managing discount coupons. Included initial SQLite database, migrations, Dockerfile, and configuration files. Updated solution and upgraded related package versions.

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,28 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<DiscountContext>(opt => 
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
+// Register services for reflection
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+if(app.Environment.IsDevelopment())
+{
+    // Add the reflection service endpoint only in development
+    app.MapGrpcReflectionService();
+}
+
+app.Run();
